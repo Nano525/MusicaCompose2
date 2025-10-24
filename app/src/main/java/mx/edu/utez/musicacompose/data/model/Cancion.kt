514c277d8cc752f65ac53245185f28af2c286c44
Vladimir-28 <20243ds028@utez.edu.mx>
2025-10-23 23:38:53 -0600

    Creacion clase cancion

--- app/src/main/java/mx/edu/utez/musicacompose/data/model/Cancion.kt
+++ app/src/main/java/mx/edu/utez/musicacompose/data/model/Cancion.kt
@@ -1,13 +1,13 @@
 package mx.edu.utez.musicacompose.data.model
 
-
-
 import androidx.annotation.DrawableRes
 
-
-data class Album (
+data class Cancion(
     val id: Int,
     val nombre: String,
     val artista: String,
+    val duracion: String,
+    val genero: String,
     @DrawableRes val imagen: Int
+
 )
