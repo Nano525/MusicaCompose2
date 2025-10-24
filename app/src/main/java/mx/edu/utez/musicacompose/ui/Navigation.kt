2bb1e2f6915a3b02dc23bf06509b8623a5aa5b03
ErnestoDof23 <20233tn194@utez.edu.mx>
2025-10-23 23:21:14 -0600

    Agregue el loginScreen y Navigation

--- app/src/main/java/mx/edu/utez/musicacompose/ui/Navigation.kt
+++ app/src/main/java/mx/edu/utez/musicacompose/ui/Navigation.kt
@@ -1,2 +1,24 @@
 package mx.edu.utez.musicacompose.ui
 
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    
+    val loginViewModel: LoginViewModel = viewModel()
+
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(loginViewModel, navController)
+        }
+        composable("passport") {
+            PassportScreen(passportViewModel, navController)
+        }
+        composable("stamp") {
+            StampScreen(passportViewModel, navController)
+        }
+    }
+}
--- app/src/main/java/mx/edu/utez/musicacompose/ui/screens/LoginScreen.kt
+++ app/src/main/java/mx/edu/utez/musicacompose/ui/screens/LoginScreen.kt
@@ -1,2 +1,63 @@
 package mx.edu.utez.musicacompose.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(viewModel: LoginViewModel, navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
+    ) {
+        CircularImage(R.drawable.utezlogo)
+        Title("Aplicación\nMóvil")
+
+        UserInputField(
+            viewModel = viewModel,
+            label = "Usuario"
+        )
+
+        PasswordField(
+            viewModel = viewModel,
+            label = "Contraseña"
+        )
+
+        if (viewModel.loginError.value.isNotEmpty()) {
+            Text(
+                text = viewModel.loginError.value,
+                color = Color.Red,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+        Link("¿Has olvidado la contraseña?") {
+            navController.navigate("forgot_password")
+        }
+
+        PrimaryButton("Iniciar sesión") {
+            navController.navigate("passport")
+        }
+
+        Link("¿No tienes cuenta? Regístrate") {
+            navController.navigate("register")
+        }
+
+    }
+}
+
