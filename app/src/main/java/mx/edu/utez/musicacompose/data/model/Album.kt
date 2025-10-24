cfe0d1cb211e46ccdd95b92fc2324fc7a3cbcf99
Nano525 <20243ds020@utez.edu.mx>
2025-10-23 23:41:08 -0600

    Error de DrawableRes

--- app/src/main/java/mx/edu/utez/musicacompose/data/model/Album.kt
+++ app/src/main/java/mx/edu/utez/musicacompose/data/model/Album.kt
@@ -3,9 +3,9 @@ package mx.edu.utez.musicacompose.data.model
 import androidx.annotation.DrawableRes
 
 
-data class Album {
+data class Album (
     val id: Int,
     val nombre: String,
     val artista: String,
     @DrawableRes val imagen: Int
-}
+    )
